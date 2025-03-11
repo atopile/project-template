71799b0d46e88cb881d2541cd14286539229ff10
Sam Mellor <sam@atopile.io>
2025-03-11 11:53:55 +0100

    Fix LICENSE.txt filename in post-gen hook

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -2,7 +2,9 @@
 import pathlib
 
 
-if __name__ == '__main__':
-
-    if 'Not open source' == '{{ cookiecutter.license }}':
-        pathlib.Path('LICENSE').unlink()
+if __name__ == "__main__":
+    if "Not open source" == "{{ cookiecutter.license }}":
+        try:
+            pathlib.Path("LICENSE").unlink()
+        except FileNotFoundError:
+            pass
